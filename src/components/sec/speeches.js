f866bcb18063df2ff8bd1a9fd36866f99905411b
Justin Shearer <justin.shearer@gmail.com>
2021-09-28 21:58:12 -0700

    sec feed v1.1

---
+++ src/components/sec/speeches.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react"
+import Parser from 'rss-parser'
+
+const News = () => {
+  const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var today = new Date();
+var todaysDay = today.getDate();
+var todaysMonth = today.getMonth();
+var searchDay = todaysDay;
+var searchMonth = months[todaysMonth].substring(0,3);
+
+let parser = new Parser();
+const rssUrl = "https://www.sec.gov/news/speeches.rss";
+const sourceUrl = "https://www.sec.gov/news/speeches-statements";
+
+const [todaysSpeeches, setTodaysSpeeches] = useState(0)
+
+useEffect(() => {
+ 
+  parser.parseURL(rssUrl, function(err, feed) {
+    var subset = [];
+    feed.items.forEach(function(entry) {
+      var entryDay = entry.pubDate.split(" ")[1]
+      var entryMonth = entry.pubDate.split(" ")[2]
+      var entryTime = entry.pubDate.split(" ")[4]
+      var entryMinute = entryTime.split(":")[1]
+      var entryHour = entryTime.split(":")[0]
+      var suffix = "";
+      if(entryHour >= 12) {
+        suffix = "PM"
+        entryHour = ((entryHour + 11) % 12 + 1)
+      } else {
+        suffix = "AM"
+      }
+      var entryTimeString = entryHour + ":" + entryMinute + suffix +" EST"
+      //if((entryDay == searchDay) && entryMonth === searchMonth){
+        subset.push (
+          <li className="768px:flex-col 768px:w-1/2 768px:px-8 py-4" key={entry.title}>
+            <a className="text-large text-navy font-bold hover:underline" href={entry.link}>{entry.title}</a>
+            <p className="opacity-90">{entry.content.replaceAll("<br />", " ")}</p>
+            <p className="text-sm opacity-25">{entryTimeString}</p>
+          </li>
+        );
+      //}
+    })
+    setTodaysSpeeches(subset)
+  })
+}, [])
+
+return (
+  <>
+      <section className={"mb-12 " + (todaysSpeeches.length > 0 ? "block" : "hidden" )}>
+        <div className="768px:float-right">
+          <a className="text-gold leading-loose" href={rssUrl}>XML</a> | <a className="inline-block text-navy" href={sourceUrl}> <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block relative -top-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg> More</a>
+        </div>
+        <h2 className="text-2xl border-b border-black-25 pb-2">Speeches</h2>
+        <ul className="768px:flex 768px:flex-wrap 768px:-mx-8">
+          {todaysSpeeches}
+        </ul>
+      </section>
+    </>
+  )
+}
+export default News
--- src/pages/sec.js
+++ src/pages/sec.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react"
 import Parser from 'rss-parser'
+import Litigation from "../components/sec/litigation"
+import News from "../components/sec/news"
+import Speeches from "../components/sec/speeches"
+import Testimony from "../components/sec/testimony"
+import Feed from "../components/sec/feed"
 
 const Sec = () => {
   const months = ["January", "February", "March", "April", "May", "June",
@@ -7,71 +12,51 @@ const Sec = () => {
 ];
 var today = new Date();
 var todaysDate = months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
-var todaysDay = today.getDate();
-var todaysMonth = today.getMonth();
-var searchDay = todaysDay;
-var searchMonth = months[todaysMonth].substring(0,3);
-
-let parser = new Parser();
-const [todaysLitigation, setTodaysLitigation] = useState(0)
-
-useEffect(() => {
-  parser.parseURL('https://www.sec.gov/rss/litigation/litreleases.xml', function(err, feed) {
-  var subset = [];
-  feed.items.forEach(function(entry) {
-    var entryDay = entry.pubDate.split(" ")[1]
-    var entryMonth = entry.pubDate.split(" ")[2]
-    if((entryDay == searchDay) && entryMonth === searchMonth){
-      subset.push (
-        <li className="block mb-2" key={entry.title}>
-          <a className="text-blue" href={entry.link}>{entry.title}</a>
-        </li>
-      );
-    }
-  });
-  setTodaysLitigation(subset)
-})
-}, [])
-
-
-
 
 return (
   <>
-      <div className="mx-auto w-3/4 font-serif">
-      <h1 className="mb-4 text-xl">What’s New on the SEC Website</h1>
-      <h2 className="mb-2 text-lg">{todaysDate}</h2>
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="1" />
+              <link
+                href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Playfair+Display&display=swap"
+                rel="stylesheet"
+              />
+
+      <header className="bg-navy text-white-50 font-serif text-center text-sm uppercase py-1 px-12">
+          Office of the Secretary of the Secretary
+      </header>
+      <div className="my-12 mx-auto max-w-2xl w-3/4 font-serif">
+      <main>
+      <h1 className="text-3xl text-navy text-center">What’s New on the SEC Website</h1>
+      <h2 className="mb-4 text-lg text-center">For {todaysDate}</h2>
+      <blockquote className="mb-4 text-sm px-6 py-4 border border-navy-light">The official What’s New page for the SEC will no longer be updated after September 30, 2021. To stay current on the most recent materials posted to the SEC website, the SEC suggests subscribing to the <a className="text-navy font-bold hover:underline" href="https://www.sec.gov/about/secrss.shtml">SEC RSS feeds</a>. <span className="font-bold">Or you can let the Secretary take of that for you.</span></blockquote>
+   
       <p className="mb-2">This page provides a daily list of the most recent materials posted to the SEC website. Note that the official release date of a document may differ from the posting date, in which case, the release date is indicated in parentheses. See also:</p>
-      <ul className="mb-2">
-        <li><a className="text-blue" href="https://www.sec.gov/about/upcoming-events.htm">Upcoming Events</a> for a list of upcoming public events including meetings, public appearances, and hearings.</li>
-        <li><a className="text-blue" href="https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent">Latest Filings</a> for the most recent company filings.</li>
-        <li><a className="text-blue" href="https://www.sec.gov/news/whatsnew/wn-archive.shtml">Previous What’s New Issues</a></li>
-        <li><a className="text-blue" href="https://www.sec.gov/news/digest.shtml">News Digest Archives (1956-2013)</a></li>
+      <ul className="mb-12 list-disc pl-8">
+        <li><a className="text-navy font-bold hover:underline" href="https://www.sec.gov/about/upcoming-events.htm">Upcoming Events</a> for a list of upcoming public events including meetings, public appearances, and hearings.</li>
+        <li><a className="text-navy font-bold hover:underline" href="https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent">Latest Filings</a> for the most recent company filings.</li>
+        <li><a className="text-navy font-bold hover:underline" href="https://www.sec.gov/news/whatsnew/wn-archive.shtml">Previous What’s New Issues</a></li>
+        <li><a className="text-navy font-bold hover:underline" href="https://www.sec.gov/news/digest.shtml">News Digest Archives (1956-2013)</a></li>
       </ul>
-      <div className="opacity-50 mb-12">
-        <h2 className="text-lg border-b-2">FOIA Program</h2>
+      
+      {/*
+      <Feed title="Press Releases" rssUrl="https://www.sec.gov/rss/news/press.xml" sourceUrl="https://www.sec.gov/news/pressreleases" />
+      <Feed title="Litigation" rssUrl="https://www.sec.gov/rss/litigation/litreleases.xml" sourceUrl="https://www.sec.gov/litigation/litreleases.htm" />
+      <Feed title="Speeches" rssUrl="https://www.sec.gov/rss/news/speech.xml" sourceUrl="https://www.sec.gov/news/speeches" />
+      <Feed title="Testimony" rssUrl="https://www.sec.gov/news/testimony.rss" sourceUrl="https://www.sec.gov/news/testimony" />
+      */}
 
-        <h2 className="text-lg border-b-2">ALJ Initial Decisions</h2>
-        <h2 className="text-lg border-b-2">ALJ Initial Orders</h2>
-        <h2 className="text-lg border-b-2">Divisions</h2>
-      </div>
-      <h2 className="text-lg border-b-2">Litigation</h2>
-        <a href="https://www.sec.gov/rss/litigation/litreleases.xml">Source</a> | <a href="https://www.sec.gov/litigation/litreleases.htm">Full Search</a>
-        <ul>
-          {todaysLitigation}
-        </ul>
-      <div>
-
-      </div>
-
+      <Feed title="Testimony" rssUrl="https://www.sec.gov/news/testimony.rss" sourceUrl="https://www.sec.gov/news/testimony" />
 
       <div className="opacity-50 mt-12">
-          <h2 className="text-lg border-b-2">News and Public Statements</h2>
-          <h2 className="text-lg border-b-2">News</h2>
-          <h2 className="text-lg border-b-2">Regulatory Actions</h2>
+      <h2 className="text-lg border-b-2">FOIA Program</h2>
+      <h2 className="text-lg border-b-2">ALJ Initial Decisions</h2>
+      <h2 className="text-lg border-b-2">ALJ Initial Orders</h2>
+      <h2 className="text-lg border-b-2">Divisions</h2>
+      <h2 className="text-lg border-b-2">News and Public Statements</h2>
+      <h2 className="text-lg border-b-2">Regulatory Actions</h2>
       </div>
-
-
+      </main>
       </div>
     </>
   )
